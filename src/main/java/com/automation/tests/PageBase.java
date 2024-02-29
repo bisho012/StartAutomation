08c97dfb91a04c0bbe0989af4f397647d32bc159
bisho012 <bishoy.ezzat87@gmail.com>
2024-02-29 15:23:40 +0400

    new pageBase file

--- src/main/java/com/automation/tests/PageBase.java
+++ src/main/java/com/automation/tests/PageBase.java
@@ -1,11 +1,12 @@
 package com.automation.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class PageBase {
 
@@ -24,6 +25,10 @@ public class PageBase {
         wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
 
+    protected void waitPageToLoad(){
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
     protected void setText(By locator, String data) {
         waitElement(locator);
         action(locator).clear();
@@ -35,10 +40,16 @@ public class PageBase {
         action(locator).click();
     }
 
-    public void selectData(By locator, int index) {
+    public void selectDataByIndex(By locator, int index) {
         Select select;
         select = new Select(action(locator));
         select.selectByIndex(index);
+    }
+
+    public void SelectDataPerform(By locator){
+        action(locator).click();
+        Actions keyDown = new Actions(driver);
+        keyDown.sendKeys(Keys.chord(Keys.DOWN, Keys.ENTER)).perform();
     }
 
     public boolean isDisplayed(By locator){
@@ -47,5 +58,54 @@ public class PageBase {
 
     public String getContent(By locator){
         return action(locator).getText();
+    }
+
+    public void scrolldown(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("javascript:window.scrollBy(0,250)");
+    }
+
+    public void scrollToElementAndClick(By locator) throws InterruptedException {
+        WebElement element = action(locator);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).click().perform();
+    }
+
+    public void refresh(){
+        driver.navigate().refresh();
+    }
+
+    public void waitUntilPageLoad(){
+        String url = driver.getCurrentUrl();
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("body")));
+    }
+
+    public void pressEnter(By locator){
+        action(locator).sendKeys(Keys.ENTER);
+    }
+    public void moveToView(By locator){
+        WebElement element = action(locator);
+        WebElement myElement = new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOf(element));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", myElement);
+    }
+
+    public void doubleClick(By locator){
+        Actions act = new Actions(driver);
+        //Double click on element
+        WebElement ele = action(locator);
+        act.doubleClick(ele).perform();
+    }
+
+    public void SelectDataPerformDoubleClick(By locator){
+        doubleClick(locator);
+        Actions keyDown = new Actions(driver);
+        keyDown.sendKeys(Keys.chord(Keys.DOWN, Keys.ENTER)).perform();
+    }
+
+    protected void Refresh(){
+        driver.navigate().refresh();
     }
 }
